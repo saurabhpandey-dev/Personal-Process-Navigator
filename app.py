e77ed6da25b2e96cee24c9324bb5cb3d0fc801e1
Saurabh Pandey <sp8355063538@gmail.com>
2026-09-04 00:26:17 +0530

    code for call login and register
    implement the route for call the login and register page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -10,5 +10,14 @@ def index():
 def dashboard():
     return render_template('dashboard.html')
 
+# this route for calling ther user login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# this route for calling the user register page
+@app.route('/register')
+def register():
+    return render_template('register.html')
 if __name__ == '__main__':
     app.run(debug=True) 

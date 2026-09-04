7c7dc1d2d511b76eb98ff3f4d04ff9f3ae407792
Saurabh Pandey <sp8355063538@gmail.com>
2026-09-04 07:57:35 +0530

    create route for login valid user
    check and create the valid user

--- app.py
+++ app.py
@@ -20,6 +20,22 @@ def dashboard():
 def login():
     return render_template('login.html')
 
+@app.route('/login_user', methods=['POST'])
+def login_user():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user_exist = db.execute('select * from user where email = ?',email) # to yha pe list of dictonary return kar rha hai 
+    if user_exist and len(user_exist) > 0:
+        store_email = user_exist[0]['email'] # index 0 pe kyu ki ek hi dict ayi hai aur uska key hai 'email' 
+        store_pass = user_exist[0]['password'] # dict ka key hai 'password'
+        if store_email == email and store_pass == password:  
+            return render_template('profile.html',data = user_exist) # ye se ham user ka basic data bhejenge 
+    else: # if email or password mismatched got print the error 
+        return render_template('login.html', error = 'Email and Password not exist') 
+    
+
+
 # this route for calling the user register page
 @app.route('/register')
 def register():
@@ -35,7 +51,7 @@ def create_user():
 
     user_exist = db.execute('select * from user where email = ?',email) # sql cammand for check USER table have the email id or not
     # if the user email exist return to the register for the another try for register
-    if len(user_exist): 
+    if len(user_exist)>0: 
         return render_template('register.html',error = 'Email alredy exits')
     
     # if email is not exits on the user table all values got stord

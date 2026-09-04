140b201181ab8edd3e116cecbb30a2f6ac45fb53
Saurabh Pandey <sp8355063538@gmail.com>
2026-09-04 06:04:14 +0530

    add register user data to db
    insert register user data to the database on the user table in name , email, phone,  password

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect
 from cs50 import SQL
 import os
 
@@ -25,6 +25,24 @@ def login():
 def register():
     return render_template('register.html')
 
+# this route for get the data from register page and fatch the db and store the data into the table 
+@app.route('/create_user', methods = ['POST']) # using the 'POST' method for not showing data on url
+def create_user():
+    name = request.form.get('name') # get the name
+    email = request.form.get('email') # get the email
+    number = request.form.get('phone') # get the number
+    password = request.form.get('password') # get the password
+
+    user_exist = db.execute('select * from user where email = ?',email) # sql cammand for check USER table have the email id or not
+    # if the user email exist return to the register for the another try for register
+    if len(user_exist): 
+        return render_template('register.html',error = 'Email alredy exits')
+    
+    # if email is not exits on the user table all values got stord
+    db.execute('insert into user (name, email, number, password) values (?, ?, ?, ?)',name,email,number,password)
+    # sql cammand for insert user data to the database user table
+    return render_template('login.html') # after inserting data go to the login page for login
+
 # this route for calling the process page
 @app.route('/process')
 def process():
@@ -39,6 +57,8 @@ def process_detail():
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
